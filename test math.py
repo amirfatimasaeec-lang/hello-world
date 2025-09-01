3091473e89a9078c5ddbacb7a5309e97c1845501
amirfatimasaeec-lang <amirfatimasaeec@gmail.com>
2025-08-31 23:01:55 -0400

    Add files via upload

---
+++ test math.py
@@ -0,0 +1,26 @@
+print("hello world")
+# Variable Fun Assignment
+
+# 1. 5 multiplied by 6
+ans1 = 5 * 6
+print("5 multiplied by 6")
+print("The answer to your query is:", ans1)
+
+# 2. 8 divided by 3
+ans2 = 8 / 3
+print("8 divided by 3")
+print("The answer to your query is:", format(ans2, ".8f"))  # 8 decimals
+
+# 3. 8 divided by 3 without the remainder
+ans3 = 8 // 3
+print("8 divided by 3 without the remainder")
+print("The answer to your query is:", ans3)
+
+# 4. 8 modulo 3 (remainder only)
+ans4 = 8 % 3
+print("8 modulo 3 (remainder only)")
+print("The answer to your query is:", ans4)
+
+# 5. 4200000 add 23090
+ans5 = 4200000 + 23090
+print("4200000 add 23090
